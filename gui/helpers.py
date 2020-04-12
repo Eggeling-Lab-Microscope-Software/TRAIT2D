39011ff698de4da405db33ef422d7f8a3f8bc40d
John Wigg <john.wigg@uni-jena.de>
2020-04-12 23:42:32 +0200

    Fit errors are now printed

---
+++ gui/helpers.py
@@ -0,0 +1,16 @@
+import numpy as np
+import math
+
+def err_format(value, error, sigdig=2):
+    if math.isnan(error):
+        error = 0
+    magn = int(np.floor(np.log10(abs(value))))
+    if error == 0:
+        dist = 0
+    else:
+        dist = int(np.floor(np.log10(abs(error))))
+
+    if (dist > magn):
+        magn = dist
+        dist += dist - magn
+    return ("{:1."+str(magn-dist+sigdig-1)+"f}({:"+str(sigdig)+".0f})E{}").format(value / 10**magn, error / 10**(dist-sigdig+1), magn)
