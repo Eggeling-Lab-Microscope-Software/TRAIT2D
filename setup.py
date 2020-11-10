0e96ca627cc9b63fa60fa0980e8aeea129674d5f
John Wigg <john.wigg@uni-jena.de>
2020-11-10 17:04:22 +0100

    add workflow for automatic publishing to pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name             = 'trait2d-john-wigg',
-    version          = '1.0-dev',
     description      = 'Cross-platform Python software package and GUIs to support Single Particle Tracking experiments.',
     long_description =long_description,
     long_description_content_type="text/markdown",
@@ -13,6 +12,11 @@ setuptools.setup(
     author_email     = 'mariia.dmitrieva@eng.ox.ac.uk, joel.lefebvre@eng.ox.ac.uk, francesco.reina@uni-jena.de, john.wigg@uni-jena.de',
     url              = "https://github.com/FReina/TRAIT-2D",
     packages         = setuptools.find_packages(),
+    version_config={
+        "version_format": "{tag}.dev{sha}",
+        "starting_version": "0.1.0"
+    },
+    setup_requires=['better-setuptools-git-version'],
     install_requires = ['setuptools', 'wheel', 'numpy', 'scipy', 'scikit-image',
                         'matplotlib', 'opencv-python', 'tqdm', 'pandas',
                         'tk', 'imageio', 'PyQt5', 'pyqtgraph'],
