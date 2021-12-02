ab92347e51c45c28e0cc545b609342f493b96849
MariiaVision <MariiaVision@users.noreply.github.com>
2021-12-02 12:02:45 +0000

    deploy: b2192bb8f8df9d2813abb2efff538dad8f4b99fc

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1-19-g55dd1b9',
+    VERSION: '1.1-20-gb2192bb',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
