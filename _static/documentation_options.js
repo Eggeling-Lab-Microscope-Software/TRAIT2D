d2b1210319c7ea090b30f5d143812ee3e626a343
MariiaVision <MariiaVision@users.noreply.github.com>
2021-12-16 23:52:28 +0000

    deploy: ed7660fa1eee7fb706a2af86f6e03c50b2e35eb7

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1-40-g430014b',
+    VERSION: '1.1-41-ged7660f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
